aa4737d6c1f7037d39a0e2ee0c699e7a5a33acef
Marco D. Mosna <marco@vouris.com>
2025-03-04 00:57:33 +0800

    feat: add mapbox

--- components/mapbox.tsx
+++ components/mapbox.tsx
@@ -3,8 +3,10 @@
 import { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useMapStore } from "@/store/map-store";
 
 export default function MapBox() {
+  const mapStore = useMapStore((state) => state); 
   const mapRef = useRef<mapboxgl.Map | null>(null);
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
@@ -14,13 +16,16 @@ export default function MapBox() {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [120.9842, 14.5995],
+        center: [123.509, 13.4337], // Buhi station 
         zoom: 10,
       });
+
+      mapStore.setMap(mapRef.current);
     }
 
     return () => {
       if (mapRef.current) {
+        mapStore.setMap(null); 
         mapRef.current.remove();
       }
     };
